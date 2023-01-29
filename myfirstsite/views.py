1ffd110684a0ac7125fbe181656ba27cabfa0b5e
Diksha <diksha.wali8383@gmail.com>
2023-01-29 22:10:20 +0530

    version 1

---
+++ myfirstsite/views.py
@@ -0,0 +1,45 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index_fun(request):
+    # return HttpResponse("<h1>Choose from the below links!</h1><br><a href='#'></a><br><a href='/newlinerem'>New line Removal</a><br><a href='/spacerem'>Space Removal</a><br><a href='/capfirst'>Capitalize First</a><br><a href='/rempunc'>Remove Punctuation</a><br><a href='/charcount'>Character Count</a>")
+    # params={'name':'Diksha Wali'}
+    # ta_text=request.GET.get('ta_text_html', 'default')
+    # print(ta_text)
+    return render(request, 'index.html')
+
+def about_fun(request):
+    return HttpResponse('This is information about our website.Enjoy!')
+
+def fileread(request):
+    with open('myfirstsite/randomfile.txt') as f:
+        lines=f.readlines()
+    return HttpResponse(f'{lines[0]}')
+
+def analyzer(request):
+    tatext=request.GET.get('tatexthtml')
+    rpdj=request.GET.get('rp')
+    ucdj=request.GET.get('uc')
+    # nlrdj=request.GET.get('nlr')
+
+    if(rpdj == 'on'):
+        for x in '''!"#$%&'()*+, -./:;<=>?@[\]^_`{|}~''':
+            tatext=tatext.replace(x, '')
+
+    if(ucdj == 'on'):
+        # print('came here!! I swear!!')
+        tatext=tatext.upper()
+
+    tatext={'text': tatext}
+    return render(request, 'analyzer.html', tatext)
+
+
+
+
+
+
+
+
+
+
+    
